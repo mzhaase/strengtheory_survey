a0de4ef67f6cf1c1c66c211f5c0663ec0a8f5c2e
Mattis <m.haase@c-store.de>
2017-01-10 09:42:33 +0100

    testing for meet numbers as well

--- weight_adjust.py
+++ weight_adjust.py
@@ -9,6 +9,7 @@
 #
 # Written for Strengtheory.com
 # Copyright Mattis Haase 2017
+import csv
 
 import numpy as np
 
@@ -25,12 +26,24 @@ womens_squat_world_record    = 278
 womens_bench_world_record    = 207
 womens_deadlift_world_record = 267
 
-# Columns
-squat_column             = 29
-squat_wrap_column        = 30
-bench_column             = 31
-deadlift_column          = 32
-total_column             = 33
+# Exercise definitions
+exercises = [
+    {'name':'meet_raw_squat_column',        'column': 19, 'type':'squat', 'value':0, 'allometric_scale':0},
+    {'name':'meet_raw_squat_wrap_column',   'column': 20, 'type':'squat', 'value':0, 'allometric_scale':0},
+    {'name':'meet_single_ply_squat_column', 'column': 21, 'type':'squat', 'value':0, 'allometric_scale':0},
+    {'name':'meet_multi_ply_squat_column',  'column': 22, 'type':'squat', 'value':0, 'allometric_scale':0},
+    {'name':'meet_raw_bench_column',        'column': 23, 'type':'bench', 'value':0, 'allometric_scale':0},
+    {'name':'meet_single_ply_bench_column', 'column': 24, 'type':'bench', 'value':0, 'allometric_scale':0},
+    {'name':'meet_multi_ply_bench_column',  'column': 25, 'type':'bench', 'value':0, 'allometric_scale':0},
+    {'name':'meet_raw_dead_column',         'column': 26, 'type':'deadlift', 'value':0, 'allometric_scale':0},
+    {'name':'meet_single_ply_dead_column',  'column': 27, 'type':'deadlift', 'value':0, 'allometric_scale':0},
+    {'name':'meet_multi_ply_dead_column',   'column': 28, 'type':'deadlift', 'value':0, 'allometric_scale':0},
+    {'name':'squat_column',                 'column': 29, 'type':'squat', 'value':0, 'allometric_scale':0},
+    {'name':'squat_wrap_column',            'column': 30, 'type':'squat', 'value':0, 'allometric_scale':0},
+    {'name':'bench_column',                 'column': 31, 'type':'bench', 'value':0, 'allometric_scale':0},
+    {'name':'deadlift_column',              'column': 32, 'type':'deadlift', 'value':0, 'allometric_scale':0},
+    {'name':'total_column',                 'column': 33, 'type':'total', 'value':0, 'allometric_scale':0}
+]
 bodyweight_column        = 4
 training_duration_column = 7
 
@@ -43,6 +56,8 @@ with open(filename, 'r') as f:
     for idx, line in enumerate(f):
         if not idx == 0:
             entries.append(line.split(','))
+        else:
+            header = line
 
 # Calculates allometric scaled strenght weight lifted * bodyweight ^ (-2/3)
 def allometric_scaled_strength(lift, bodyweight):
@@ -50,32 +65,29 @@ def allometric_scaled_strength(lift, bodyweight):
 
 # convert weight to kg
 def convert_to_kg(weight):
-    return(weight/kg_to_lbs)
+    return(str(weight/kg_to_lbs))
 
 # heuristic to check if it is wrong unit
-def is_wrong_unit(bodyweight, training_duration, sex, squat, squat_wrap, bench, deadlift, total):
+def is_wrong_unit(bodyweight, training_duration, sex, exercises):
     # first, filter for world records
-    if sex == 'f':
-        if squat >= womens_squat_world_record: return True
-        if bench >= womens_bench_world_record: return True
-        if deadlift >= womens_deadlift_world_record: return True
-    if sex == 'm':
-        if squat >= mens_squat_world_record: return True
-        if bench >= mens_bench_world_record: return True
-        if deadlift >= mens_deadlift_world_record: return True
-    if training_duration < 5: # if training for less than 2 years
-        allometric_squat      = allometric_scaled_strength(squat, bodyweight)
-        allometric_squat_wrap = allometric_scaled_strength(squat_wrap, bodyweight)
-        allometric_bench      = allometric_scaled_strength(bench, bodyweight)
-        allometric_deadlift   = allometric_scaled_strength(deadlift, bodyweight)
-        allometric_total      = allometric_scaled_strength(total, bodyweight)
+    for exercise in exercises:
+        if exercise['type'] == 'squat':
+            if sex == 2 and exercise['value'] > womens_squat_world_record: return True
+            if sex == 1 and exercise['value'] > mens_squat_world_record: return True
+        if exercise['type'] == 'bench':
+            if sex == 2 and exercise['value'] > womens_bench_world_record: return True
+            if sex == 1 and exercise['value'] > mens_bench_world_record: return True
+        if exercise['type'] == 'deadlift':
+            if sex == 2 and exercise['value'] > womens_deadlift_world_record: return True
+            if sex == 1 and exercise['value'] > mens_deadlift_world_record: return True
 
-        # check whether lifts are above allometric scaled strength cutoff points
-        if allometric_squat      >= squat_cutoff   : return True
-        if allometric_squat_wrap >= squat_cutoff   : return True
-        if allometric_bench      >= bench_cutoff   : return True
-        if allometric_deadlift   >= deadlift_cutoff: return True
-        if allometric_total      >= total_cutoff   : return True
+    if training_duration < 5: # if training for less than 2 years
+        for idx, exercise in enumerate(exercises):
+            exercise['allometric_scale'] = allometric_scaled_strength(exercise['value'], bodyweight)
+            if exercise['type'] == 'squat' and exercise['allometric_scale'] > squat_cutoff: return True
+            if exercise['type'] == 'bench' and exercise['allometric_scale'] > bench_cutoff: return True
+            if exercise['type'] == 'deadlift' and exercise['allometric_scale'] > deadlift_cutoff: return True
+            if exercise['type'] == 'total' and exercise['allometric_scale'] > total_cutoff: return True
 
 changed_entries = []
 
@@ -84,26 +96,11 @@ for idx, entry in enumerate(entries):
     # get all relevant colums. If column is empty, use 1 instead.
     training_duration = int(entry[training_duration_column])
     sex = 'm' if entry[1] == 1 else 'f'
-    try:
-        squat = float(entry[squat_column])
-    except:
-        squat = 1
-    try:
-        squat_wrap = float(entry[squat_wrap_column])
-    except:
-        squat_wrap = 1
-    try:
-        bench = float(entry[bench_column])
-    except:
-        bench = 1
-    try:
-        deadlift = float(entry[deadlift_column])
-    except:
-        deadlift = 1
-    try:
-        total = float(entry[total_column])
-    except:
-        total = 1
+    for exercise_idx, exercise in enumerate(exercises):
+        try:
+            exercises[exercise_idx]['value'] = float(entry[exercise['column']])
+        except:
+            exercises[exercise_idx]['value'] = 1
     #print(idx, entry[bodyweight_column])
     try:
         bodyweight = float(entry[bodyweight_column])
@@ -111,17 +108,17 @@ for idx, entry in enumerate(entries):
         bodyweight = 1
 
     # check if wrong unit using heuristic, if yes, convert all lifts to kg
-    if is_wrong_unit(bodyweight, training_duration, sex, squat, squat_wrap, bench, deadlift, total):
-        entries[idx][squat_column]      = convert_to_kg(squat)
-        entries[idx][squat_wrap_column] = convert_to_kg(squat_wrap)
-        entries[idx][bench_column]      = convert_to_kg(bench)
-        entries[idx][deadlift_column]   = convert_to_kg(deadlift)
-        entries[idx][total_column]      = convert_to_kg(total)
-        changed_entries.append(idx + 2)
+    if is_wrong_unit(bodyweight, training_duration, sex, exercises):
+        for exercise in exercises:
+            # ignore placeholder 1
+            if not exercise['value'] == 1:
+                entries[idx][exercise['column']] = convert_to_kg(exercise['value'])
+                if not idx + 2 in changed_entries: changed_entries.append(idx + 2)
 
 print(changed_entries)
-
-# Write entries
-# with open('survey_adjusted.csv', 'w') as f:
-#     for entry in entries:
-#         f.write(entry)
+#print(entries[4])
+# Write entries to file
+with open('survey_adjusted.csv', 'w') as f:
+    f.write(header)
+    for entry in entries:
+        f.write(','.join(entry).replace("'",""))
